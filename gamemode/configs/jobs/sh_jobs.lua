2bfecd679a6c0b1770ad191a9ce86a26c4885675
Nyaaa <5182757+oNyaaa1@users.noreply.github.com>
2025-10-28 13:57:52 +0000

    update

--- gamemode/configs/jobs/sh_jobs.lua
+++ gamemode/configs/jobs/sh_jobs.lua
@@ -8,16 +8,15 @@ sTub.Jobs.Config = function(job, code)
     }
 end
 
-sTub.Jobs.Config("Citizen", function(ply, job, code)
+sTub.Jobs.Config("Citizen", function()
     return {
         color = Color(245, 245, 0),
         VIP = false,
-        VIP_LIST = ply:GetUserGroup("vip"),
+        VIP_LIST = function(ply) ply:GetUserGroup("vip") end,
         model = {"models/player/Group01/Female_01.mdl", "models/player/Group01/Female_02.mdl", "models/player/Group01/Female_03.mdl", "models/player/Group01/Female_04.mdl", "models/player/Group01/Female_06.mdl", "models/player/group01/male_01.mdl", "models/player/Group01/Male_02.mdl", "models/player/Group01/male_03.mdl", "models/player/Group01/Male_04.mdl", "models/player/Group01/Male_05.mdl", "models/player/Group01/Male_06.mdl", "models/player/Group01/Male_07.mdl", "models/player/Group01/Male_08.mdl", "models/player/Group01/Male_09.mdl"},
         JobType = "Citizen",
         description = [[The Classic roleplay citizen life!]],
         weaponz = {"choc_hands"},
-        command = "citizen",
         max = 0,
         salary = 0,
         admin = 0,
@@ -26,16 +25,15 @@ sTub.Jobs.Config("Citizen", function(ply, job, code)
     }
 end)
 
-sTub.Jobs.Config("Robbers", function(ply, job, code)
+sTub.Jobs.Config("Robbers", function()
     return {
         color = Color(255, 0, 0),
         VIP = false,
-        VIP_LIST = ply:GetUserGroup("vip"),
+        VIP_LIST = function(ply) ply:GetUserGroup("vip") end,
         model = {"models/player/Group01/Female_01.mdl", "models/player/Group01/Female_02.mdl", "models/player/Group01/Female_03.mdl", "models/player/Group01/Female_04.mdl", "models/player/Group01/Female_06.mdl", "models/player/group01/male_01.mdl", "models/player/Group01/Male_02.mdl", "models/player/Group01/male_03.mdl", "models/player/Group01/Male_04.mdl", "models/player/Group01/Male_05.mdl", "models/player/Group01/Male_06.mdl", "models/player/Group01/Male_07.mdl", "models/player/Group01/Male_08.mdl", "models/player/Group01/Male_09.mdl"},
-        JobType = "Citizen",
+        JobType = "Robbers",
         description = [[The Classic roleplay citizen life!]],
         weaponz = {"choc_hands"},
-        command = "citizen",
         max = 0,
         salary = 0,
         admin = 0,
@@ -44,16 +42,15 @@ sTub.Jobs.Config("Robbers", function(ply, job, code)
     }
 end)
 
-sTub.Jobs.Config("Police", function(ply, job, code)
+sTub.Jobs.Config("Police", function()
     return {
         color = Color(30, 0, 255),
         VIP = false,
-        VIP_LIST = ply:GetUserGroup("vip"),
+        VIP_LIST = function(ply) ply:GetUserGroup("vip") end,
         model = {"models/player/Group01/Female_01.mdl", "models/player/Group01/Female_02.mdl", "models/player/Group01/Female_03.mdl", "models/player/Group01/Female_04.mdl", "models/player/Group01/Female_06.mdl", "models/player/group01/male_01.mdl", "models/player/Group01/Male_02.mdl", "models/player/Group01/male_03.mdl", "models/player/Group01/Male_04.mdl", "models/player/Group01/Male_05.mdl", "models/player/Group01/Male_06.mdl", "models/player/Group01/Male_07.mdl", "models/player/Group01/Male_08.mdl", "models/player/Group01/Male_09.mdl"},
-        JobType = "Citizen",
+        JobType = "Police",
         description = [[The Classic roleplay citizen life!]],
         weaponz = {"choc_hands"},
-        command = "citizen",
         max = 0,
         salary = 0,
         admin = 0,
@@ -62,16 +59,15 @@ sTub.Jobs.Config("Police", function(ply, job, code)
     }
 end)
 
-sTub.Jobs.Config("VIP - Citizen", function(ply, job, code)
+sTub.Jobs.Config("VIP - Citizen", function()
     return {
         color = Color(0, 255, 247),
         VIP = true,
-        VIP_LIST = ply:GetUserGroup("vip"),
+        VIP_LIST = function(ply) ply:GetUserGroup("vip") end,
         model = {"models/player/Group01/Female_01.mdl", "models/player/Group01/Female_02.mdl", "models/player/Group01/Female_03.mdl", "models/player/Group01/Female_04.mdl", "models/player/Group01/Female_06.mdl", "models/player/group01/male_01.mdl", "models/player/Group01/Male_02.mdl", "models/player/Group01/male_03.mdl", "models/player/Group01/Male_04.mdl", "models/player/Group01/Male_05.mdl", "models/player/Group01/Male_06.mdl", "models/player/Group01/Male_07.mdl", "models/player/Group01/Male_08.mdl", "models/player/Group01/Male_09.mdl"},
-        JobType = "Citizen",
+        JobType = "VIP - Citizen",
         description = [[The Classic roleplay citizen life!]],
         weaponz = {"choc_hands"},
-        command = "citizen",
         max = 0,
         salary = 0,
         admin = 0,
@@ -81,7 +77,7 @@ sTub.Jobs.Config("VIP - Citizen", function(ply, job, code)
 end)
 
 local meta = FindMetaTable("Player")
-function IsValidJob(name)
+function IsValidJob(name, ply)
     local torf = false
     local atbl = {}
     for k, v in pairs(sTub.Jobs.Registered) do
@@ -93,12 +89,17 @@ function IsValidJob(name)
             end
         end
     end
-    return {atbl(), torf}
+    return {atbl(ply), torf}
 end
 
 function meta:SetJob(name)
-    local job = IsValidJob(name)
+    local job = IsValidJob(name, self)
     if not job[2] then return end
+    if job[1].VIP == true and job[1].VIP_LIST then
+        LoggerDev(Format("%s Tried getting a vip Job", self:Nick()))
+        return
+    end
+
     for k, v in pairs(job[1]) do
         if k == "model" then self:SetModel(table.Random(v)) end
         if k == "weaponz" then
